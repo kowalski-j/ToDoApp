b5979e51d95b3028a445cd78f2027b03e2820e12
Lucky Ebhoaye <lucky.ebhoaye@cyberspace.net.ng>
2022-10-20 10:42:26 +0100

    Reload page upon events

--- ToDoApp-SPA/src/app/components/edit-agenda/edit-agenda.component.ts
+++ ToDoApp-SPA/src/app/components/edit-agenda/edit-agenda.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Agenda } from 'src/app/models/agenda';
 import { AgendaService } from 'src/app/services/agenda.service';
+import { AlertifyService } from 'src/app/services/alertify.service';
 
 @Component({
   selector: 'app-edit-agenda',
@@ -11,7 +12,7 @@ export class EditAgendaComponent implements OnInit {
 @Input() agenda? : Agenda;
 @Output() agendaUpdated = new EventEmitter<Agenda[]>();
 
-  constructor(private agendaService: AgendaService) { }
+  constructor(private agendaService: AgendaService, private alertify: AlertifyService) { }
 
   ngOnInit(): void {
   }
@@ -19,21 +20,25 @@ export class EditAgendaComponent implements OnInit {
   updateAgenda(agenda?: Agenda){
     this.agendaService
     .updateAgenda(agenda)
-    .subscribe((agendas: Agenda[]) => this.agendaUpdated.emit(agendas));  
-
-  }
+    .subscribe(() => {
+      window.location.reload();
+    });
+ }
 
   deleteAgenda(agenda?: Agenda){
     this.agendaService
     .deleteAgenda(agenda)
-    .subscribe((agendas: Agenda[]) => this.agendaUpdated.emit(agendas));  
-  }
+    .subscribe(() => {
+      window.location.reload();
+    });
+ }
   
   createAgenda(agenda?: Agenda){
     this.agendaService
     .createAgenda(agenda)    
-    .subscribe((agendas: Agenda[]) => this.agendaUpdated.emit(agendas));  
-
+    .subscribe(() => {
+      window.location.reload();
+    });
  }
 
 
